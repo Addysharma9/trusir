38908d8d9e13e8262d8b24bd50ee95b3a7537bb3
Addysharma9 <codewithaddysharma@gmail.com>
2024-10-26 20:44:04 +0530

    tutor-done

--- resources/views/star.blade.php
+++ resources/views/star.blade.php
@@ -1,19 +1,67 @@
 <div class="star-100">
-<div class="star-90" >
-    <div class="foty-forty" >
-        <div class="forty">
+    <div class="star-901">
+        <div class="text-40">
 
-            <h1>Our <span class="highlight">Star Tutors</span>  brings a   wealth of <br>  experience</h1>
+            <h1>Our <span class="highlight">Star Tutors</span> brings a wealth of experience</h1>
         </div>
-        <div class="forty1">
+        <div class="text-50">
 
-          
             <p>Meet our star tutors – the backbone of Trusir. Handpicked for their expertise and passion, they're not just teachers but mentors who bring out the best in our students. With proven track records in their subjects, our tutors are here to guide you towards academic success.</p>
         </div>
-       
-    </div>
-    <div class="teacher">
-
     </div>
 </div>
+
+<div class="star-100">
+    <div class="star-90">
+        
+        <!-- Teacher Cards Container -->
+        <div class="teacher-container">
+            <div class="profile-card">
+                <div class="profile-image-container">
+                    <img src="images/teacher1.png" alt="Teacher 1">
+                </div>
+                <div class="profile-details">
+                    <h3 class="profile-name">Teacher Name 1</h3>
+                    <p class="profile-title">Math Teacher</p>
+                </div>
+            </div>
+            <div class="profile-card">
+                <div class="profile-image-container1">
+                    <img src="images/teacher2.png" alt="Teacher 2">
+                </div>
+                <div class="profile-details">
+                    <h3 class="profile-name">Teacher Name 2</h3>
+                    <p class="profile-title">Science Teacher</p>
+                </div>
+            </div>
+            <div class="profile-card">
+                <div class="profile-image-container2">
+                    <img src="images/teacher3.png" alt="Teacher 3">
+                </div>
+                <div class="profile-details">
+                    <h3 class="profile-name">Teacher Name 3</h3>
+                    <p class="profile-title">English Teacher</p>
+                </div>
+            </div>
+            <div class="profile-card">
+                <div class="profile-image-container3">
+                    <img src="images/teacher4.png" alt="Teacher 4">
+                </div>
+                <div class="profile-details">
+                    <h3 class="profile-name">Teacher Name 4</h3>
+                    <p class="profile-title">History Teacher</p>
+                </div>
+            </div>
+            <div class="profile-card">
+                <div class="profile-image-container4">
+                    <img src="images/teacher5.png" alt="Teacher 5">
+                </div>
+                <div class="profile-details">
+                    <h3 class="profile-name">Teacher Name 5</h3>
+                    <p class="profile-title">Economics Teacher</p>
+                </div>
+            </div>
+            
+        </div>
+    </div>
 </div>
